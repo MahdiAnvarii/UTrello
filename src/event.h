559932c3950036d94cd9beef7f4949a6a3cad716
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2025-05-28 16:25:13 +0330

    Added: Event/Task classes for scheduling system
    - Base Event class with time/date management
    - PeriodicEvent for recurring schedules (daily/weekly/monthly)
    - Task class with editing capability
    - Sorting utilities for all job types
    - Reporting functionality for each class

---
+++ src/event.h
@@ -0,0 +1,62 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+#include "utils.h"
+#include "date.h"
+
+class Event{
+public:
+    Event(shared_ptr<Date> eventDate_, int eventCounter_, int start_time_, int duration_, string title_, string description_);
+    shared_ptr<Date> getEventDate() const;
+    int getEventStartTime() const;
+    int getEventDuration() const;
+    int getEventID() const;
+    void reportEvent();
+private:
+    shared_ptr<Date> eventDate;
+    int eventID;
+    int start_time;
+    int duration; 
+    string title;
+    string description;
+};
+
+class PeriodicEvent{
+public:
+    PeriodicEvent(vector<shared_ptr<Date>> periodicEventDates_, int periodicEventCounter_, int start_time_, int duration_, 
+                    PeriodicType periodicType_, string title_, string description_);
+    vector<shared_ptr<Date>> getPeriodicEventDates() const;
+    int getPeriodicEventStartTime() const;
+    int getPeriodicEventDuration() const;
+    int getPeriodicEventID() const;
+    void reportPeriodicEvent(shared_ptr<Date> periodicEventDate);
+private:
+    vector<shared_ptr<Date>> periodicEventDates;
+    int periodicEventID;
+    int start_time; 
+    int duration;
+    PeriodicType periodicType; 
+    string title;
+    string description;
+};
+
+class Task{
+public:
+    Task(shared_ptr<Date> taskDate_, int taskCounter_, int time_, string title_, string description_);
+    void editTask(string dateLine_, int time_, string title_, string description_);
+    shared_ptr<Date> getTaskDate() const;
+    int getTaskID() const;
+    void reportTask();
+private:
+    shared_ptr<Date> taskDate;
+    int taskID;
+    int time;
+    string title;
+    string description;
+};
+
+void sortEvents(vector<shared_ptr<Event>>& events);
+void sortPeriodicEvents(vector<shared_ptr<PeriodicEvent>>& periodicEvents);
+void sortTasks(vector<shared_ptr<Task>>& tasks);
+
+#endif //EVENT_H
